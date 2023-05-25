48afade1393624bd97e43dca797b9a2ab1b33c27
Gerenzeo <124388411+Gerenzeo@users.noreply.github.com>
2023-05-25 21:49:36 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,207 +1,230 @@
-from collections import UserDict
 from datetime import datetime
-import json
-from pathlib import Path
-import sys
+import random
 
-class ValidatePhone(Exception):
-    pass
-class ValidateBirthday(Exception):
-    pass
+from logic import AddressBook, Record, ValidateBirthday, ValidatePhone
 
-class AddressBook(UserDict):
-    filename = Path('contacts.json')
 
-    def __init__(self, data=None):
-        super().__init__(data)
-        self.elements_per_page = 5
-        self.current_page = 1
-    
-    def add_record(self, record):
-        self.data[record.name.value] = record
 
-    def __iter__(self):
-        self.current_page = 1
-        return self
+CONTACTS = AddressBook()
 
-    def __next__(self):
-        start_index = (self.current_page - 1) * self.elements_per_page
-        end_index = start_index + self.elements_per_page
-        keys = list(self.data.keys())
-        if start_index >= len(keys):
-            raise StopIteration
-        page_keys = keys[start_index:end_index]
-        page_records = {key: self.data[key] for key in page_keys}
-        self.current_page += 1
-        return page_records
 
-    def serialize(self):
-        data_list = []
-        for _, record in self.data.items():
-            d = {record.name.value: {'name': record.name.value, 'phones': record.phones, 'birthday': f'{str(record.birthday)}'}}
-            data_list.append(d)
-
-        with open(self.filename, 'w') as file:
-            json.dump(data_list, file, indent=4)
-
-        return data_list
-    
-    def deserialize(self):
+# DECORATOR
+def input_error(func):
+    def inner(*args, **kwargs):
         try:
-            with open(self.filename, 'r') as file:
-                content = json.load(file)
-                for elements in content:
-                    for key, value in elements.items():
-                        rec = Record(value['name'])
-                        for phone in value['phones']:
-                            rec.add_phone(phone)
-                        
-                        rec.birthday = value['birthday']
-                    self.add_record(rec)
-        except FileNotFoundError:
-            pass
-    
-    def search(self, word):
-        users = []
-        if self.data is {}:
-            return 'You data is empty'
+            result = func(*args, **kwargs)
+            return result
+        except KeyError as e:
+            return f"User {e} is not exist! Please add user before using this command!"
+        except ValueError:
+            return 'Give me name and phone please!'
+        except IndexError:
+            return 'You enter not corrent name or this name is not exist!'
+        except TypeError:
+            return 'Please enter name and phone!'
+        except ValidateBirthday as e:
+            return f'{e} - You enter not validate birthday! Must be <day>.<month>.<year>'
+        except ValidatePhone:
+            return 'You enter not validate phone! Must started with [+380, 380, 80, 0] and length of number = 13'
+        except StopIteration:
+            return 'Pagination end!'
+    return inner
+
+def command_hello(*args):
+    return 'How can I help you?'
+
+def command_load(contacts: AddressBook):
+    contacts.deserialize()
+    return ''
+
+def command_save(contacts: AddressBook):
+    if not contacts:
+        return ''
+    contacts.serialize()
+    return ''
+
+def command_search(contacts: AddressBook, *args):
+    return contacts.search(args[0])
+
+@input_error
+def command_add_user(contacts: dict, name, *args):
+    if name in contacts:
+        return f'Sorry, but contact with name {name.title()} already exist! Please try another name!'
+    else:
+        record = Record(name)
         
-        for key, value in self.data.items():
-            mini_s = f'{value.name}{"".join(value.phones)}' 
-            if mini_s.find(word) != -1:
-                users.append(key)
+        if args:
+            for phone in args:
+                record.add_phone(phone)
         
-        if len(users) > 0:
-            return users
-        
-        return f'No results found for “{word}”'
+        contacts.add_record(record)
+        return f'User {name.title()} successfully added!'
 
 
-# ----
-class Field:
-    def __init__(self, value=None):
-        self.value = value
+@input_error
+def command_add_phone(contacts: dict, name, phone):
+    if name not in contacts:
+        raise KeyError(name)
+    record = contacts[name]
+
+    if phone in record.phones:
+        return f'Sorry, but phone with number {phone} already exist!'
+
+    record.add_phone(phone)
+    contacts.add_record(record)
+    return f'Phone {phone} successfully added for user {str(name).title()}.'
+
+
+@input_error
+def command_set_birthday(contacts: dict, name, birthday):
+    if name not in contacts:
+        raise KeyError(name)
     
-    def __str__(self):
-        return str(self.value)
-        
-class Name(Field):
-    pass
+    record = contacts[name]
 
-class Phone(Field):
-    @property
-    def phone(self):
-        return self.value
+    splitter = None
+    for char in birthday:
+        if char in ['.', '/', '-', ',', ' ']:
+            splitter = char
+            break
+
+    if splitter != None:
+        days, month, year = birthday.split(splitter)
+
+        birthday = datetime(day=int(days), month=int(month), year=int(year)).date()
+    else:
+        raise ValidateBirthday(birthday)
     
-    @phone.setter
-    def phone(self, new_phone):
-        self.value = new_phone
+    record.birthday = birthday
+    contacts.add_record(record)
+    return f'Birthday successfully set for user {str(name).title()}.'
+
+@input_error
+def command_days_to_birthday(contacts: dict, name):
+    if name not in contacts:
+        raise KeyError(name)
     
-    def normalize(self):
-        phone = self.value
+    record = contacts[name]
+    record.days_to_birthday()
+    return ''
 
-        if len(phone) == 13 and phone.startswith('+380'):
-            number = phone
-        elif len(phone) == 12 and phone.startswith('380'):
-            number = '+' + phone
-        elif len(phone) == 11 and phone.startswith('80'):
-            number = '+3' + phone
-        elif len(phone) == 10 and phone.startswith('0'):
-            number = '+38' + phone
-        else:
-            raise ValidatePhone('This is not correct phone number!')
-    
-        return number
-
-    def __str__(self):
-        return str(self.value)
-
-class Birthday(Field):
-    @property
-    def birthday(self):
-        if self.value is None:
-            return None
-        
-        splitter = None
-        for char in self.value:
-            if char in ['.', '/', '-', ',', ' ']:
-                splitter = char
-                break
-        
-        if splitter != None:
-            days, month, year = self.value.split(splitter)
-
-            return datetime(day=int(days), month=int(month), year=int(year)).date()
-                
-        raise ValidateBirthday('Not validate birthday!')
-    
-    @birthday.setter
-    def birthday(self, date):
-        self.value = date
-        
-    def __str__(self):
-        return str(self.value)
-    
-class Record:
-    def __init__(self, name, birthday=None):
-        self.name = Name(name)
-        self.phones = []
-        self.birthday = Birthday(birthday).birthday
-
-    def add_phone(self, phone):
-        for el in self.phones:
-            if el == phone:
-                return f'Phone with number {phone} already exist!'
-        
-        self.phones.append(str(Phone(phone.normalize()).phone))
-        return f'Phone {phone} successfuly added!'
-    
-    def remove_phone(self, phone):
-        if phone in self.phones:
-            for index, el in enumerate(self.phones):
-                if el == phone:
-                    del self.phones[index]
-                    return f'Phone {phone} was deleted!'
-
-        return f'Sorry this number is not exist!' 
-
-    def edit_phone(self, old_phone, new_phone):
-        if old_phone in self.phones:
-            for index, number in enumerate(self.phones):
-                if old_phone == number:
-                    self.phones[index] = Phone(new_phone).phone
-                    return f'Phone with number {old_phone} successfully change at {new_phone}'
-            
-        return f'Number with {old_phone} not found!'
-    
-    def days_to_birthday(self):
-        if self.birthday is None:
-            print('You need set birthday first!')
-            return ''
-        
-        splitter = None
-        for char in str(self.birthday):
-            if char in ['.', '/', '-', ',', ' ']:
-                splitter = char
-                break
-                
-        user_year, user_month, user_day = str(self.birthday).split(splitter)
-        today = datetime.today()
-        user_birthday = datetime(year=int(user_year), month=int(user_month), day=int(user_day))
-
-        delta_user_birthday = user_birthday.replace(year=today.year)
-        if delta_user_birthday < today:
-            delta_user_birthday = delta_user_birthday.replace(year=today.year + 1)
-
-        delta = delta_user_birthday - today
-        delta_days = delta.days
-
-        print(f"To birthday {int(delta_days)+1} days!")
+@input_error
+def command_view_contacts(contacts: dict):
+    if len(contacts) < 5:
+        command_show_all(contacts)
+    else:
+        for name in contacts.__next__():
+            record = contacts[name]
+            print(f'{record.name.value}')
+            print(f'   Phones: {", ".join([p.phone for p in record.phones])}')
+            print(f'   Birthday: {record.birthday}')
+            print('---------')
         return ''
 
-    def __repr__(self) -> str:
-        return f'Record({self.name}, {self.phones}, {self.birthday})'
+@input_error
+def command_delete_phone(contacts: dict, name, phone):
+    if name not in contacts:
+        return f'Sorry but {name} not found!'
     
+    record = contacts[name]
+
+    if phone in record.phones:
+        record.remove_phone(phone)
+        contacts.add_record(record)
+        return f'Phone {phone} successfully deleted for user {str(name).title()}.'
+    
+    return f'User {str(name).title()} don\'t have this number {phone}'
+
+@input_error
+def command_change(contacts: dict, name, old_phone, new_phone):
+    if name not in contacts:
+        raise KeyError(name)
+    
+    record = contacts[name]
+    record.edit_phone(old_phone, new_phone)
+    contacts.add_record(record)
+    return f'Phone number {old_phone} has been changed to {new_phone} for {str(name).title()}.'
+
+@input_error
+def command_phone(contacts: dict, name):
+    if name not in contacts:
+        raise KeyError(name)
+    record = contacts[name]
+    
+    print(f'{str(record.name.value).title()}')
+    print(f'   Phones: {", ".join([phone for phone in record.phones])}')
+    print('---------')
+    return ''
+
+@input_error
+def command_generate_contacts(contacts: dict, count_contacts):
+    if len(contacts) == 0:
+        for i in range(int(count_contacts)):
+            number = '+380'
+            number += str(random.choices(['66', '50', '63', '96', '93', '67'])[0])
+            number += ''.join([str(random.randint(0, 9)) for _ in range(7)])
+            print(command_add_user(contacts, f'user_{i+1}', number))
+    else:
+        print('Please restart your app! And generate contacts when you dont have any contacts!')
+            
+    
+    return ''
+
+
+def command_show_all(contacts: AddressBook):
+    print(contacts.show_all())
+    return ''
+
+
+def command_unknown(command):
+    return f'Command [{command}] is not exist!'
+
+COMMANDS = {
+    'hello': command_hello,
+    'search': command_search,
+    'add user': command_add_user,
+    'add phone': command_add_phone,
+    'delete phone': command_delete_phone,
+    'set birthday': command_set_birthday,
+    'days to birthday': command_days_to_birthday,
+    'view': command_view_contacts,
+    'change': command_change,
+    'phone': command_phone,
+    'generate': command_generate_contacts,
+    'show all': command_show_all
+}
+
+def main():
+    print('For quick test. We can use command [generate <count contacts>]')
+    command_load(CONTACTS)
+    while True:
+        command = input('Enter command: ').lower()
+        if command in ['exit', 'good bye', 'close']:
+            command_save(CONTACTS)
+            print('Good bye!')
+            break
+        if command:
+            current_command = filter(lambda comanda: command.startswith(comanda), [c for c in COMMANDS.keys()])
+            func = ''
+            
+            try:
+                func = command[:len(list(current_command)[0])]
+            except IndexError:
+                pass
+            
+            if COMMANDS.get(func):
+                handler = COMMANDS.get(func)
+                data = command[len(func):].strip()
+
+                if data:  
+                    data = data.split(' ')
+                result = handler(CONTACTS, *data)
+            else:
+                result = command_unknown(command)  
+            print(result, '\n')
+        else:
+            print('Please write something!', '\n')
+
 
 if __name__ == '__main__':
-    pass
+    main()
